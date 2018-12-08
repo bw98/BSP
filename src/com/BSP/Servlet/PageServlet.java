d14d6d2b0e6d8932a200d9bb2043e313ddee6dd1
ooorange-l <15291012963@163.com>
2018-12-08 09:54:33 +0800

    page

---
+++ src/com/BSP/Servlet/PageServlet.java
@@ -0,0 +1,46 @@
+package com.BSP.Servlet;
+
+import com.BSP.Service.PageService;
+import com.BSP.bean.Book;
+import com.BSP.bean.Page;
+import net.sf.json.JSONArray;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+
+public class PageServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        request.setCharacterEncoding("utf-8");
+                response.setContentType("text/html;charset=utf-8");
+                /*
+                 * 1.获取当前页数
+                 * 2.设置显示条数
+                 * 3.根据pageservice的findbookbypage方法获得list
+                 * 4.将list存入request域中；
+                 */
+                int pagenum=Integer.valueOf(request.getParameter("pagenum"));
+                int pagesize=8;
+                PageService pageService = new PageService();
+               Page page=pageService.findbookbypage(pagenum,pagesize);
+                List<Book> list=page.getList();
+                String json=JSONArray.fromObject(list).toString();
+                  response.getWriter().print(json);
+                response.getWriter().print(1);
+                // System.out.println(json);
+
+            }
+
+
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request,response);
+    }
+}
