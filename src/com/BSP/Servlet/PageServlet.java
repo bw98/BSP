efa6a4eba8d6d3f7407033978c5d6bb9dcdf7fc9
ooorange-l <15291012963@163.com>
2019-06-12 11:44:03 +0800

    时间类型修改

--- src/com/BSP/Servlet/PageServlet.java
+++ src/com/BSP/Servlet/PageServlet.java
@@ -2,10 +2,12 @@ package com.BSP.Servlet;
 
 import com.BSP.Service.BookService;
 import com.BSP.Service.PageService;
+import com.BSP.Util.JsonDateValueProcessor2;
 import com.BSP.bean.Book;
 import com.BSP.bean.Page;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+import net.sf.json.JsonConfig;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
@@ -15,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Date;
 import java.util.List;
 
 
@@ -48,7 +51,10 @@ public class PageServlet extends HttpServlet {
         PageService pageService = new PageService();
         Page page = pageService.findbookbypage(pagenum, pagesize);
         List<Book> list = page.getList();
-        String json = JSONArray.fromObject(list).toString();
+        JsonConfig config = new JsonConfig();
+        JsonDateValueProcessor2 jsonDateValueProcessor = new JsonDateValueProcessor2();
+        config.registerJsonValueProcessor(Date.class, jsonDateValueProcessor);
+        String json = JSONArray.fromObject(list, config).toString();
         response.getWriter().print(json);
     }
 
